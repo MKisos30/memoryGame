5959adcf09d805f3ba03a6bceb7e74416809da0a
Miriam Kisos <87091443+MKisos30@users.noreply.github.com>
2022-10-02 20:10:18 +0300

    aaa

---
+++ frontend/src/Data/cards.js
@@ -0,0 +1,17 @@
+exports.gameCards = [
+    {
+        src: "/pict/1.jpeg",
+        id: "11",
+        alt: "flowers1",
+    },
+    {
+        src: "/pict/2.jpeg",
+        id: "22",
+        alt: "flowers2",
+    },
+    {
+        src: "/pict/3.jpeg",
+        id: "33",
+        alt: "flowers3",
+    },
+]
--- frontend/src/Routes/GamePage.js
+++ frontend/src/Routes/GamePage.js
@@ -1,9 +1,63 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { gameCards } from '../Data/cards';
 
 const GamePage = ({nameUser}) => {
+  const [cardForGame, setCardForGame] = useState([])
+
+  function newArrCard() {
+    const cards = [];
+
+    //const countOfcopies = 2
+
+    for (let i = 0; i < 2; i++){
+      for (let j = 0; j < gameCards.length; j++) {
+        cards.push(gameCards[j])
+      }
+    } 
+
+    cards.sort(() => Math.round(Math.random() * 100 ) - 50 )
+    setCardForGame(cards)
+  }
+
+  useEffect(() => {
+    newArrCard(gameCards)
+  }, [])
+
+  let userCarDChoose = []
+
+  const clickCard = (id) => {
+    if (userCarDChoose.length === 0 || userCarDChoose.length === 1) {
+            //להפוך את הכרטיסים
+      userCarDChoose.push(id)
+      checkCards(userCarDChoose)
+    } 
+  }
+  
+  const checkCards = (playerCards) => {
+    console.log(playerCards)
+    if(playerCards.length===2) {
+      if ( playerCards[0] === playerCards[1]) {
+        alert('It is same')
+      } else {
+        alert('It is diffrent')
+      }
+      userCarDChoose = []
+    } 
+    // else {
+    //   console.log('it is false, need to rotate back')
+    // } 
+  }
+
   return (
     <div>
       <h1>Welcome {nameUser}!</h1>
+      <div>
+        {cardForGame.map((card, index) => {
+          return (
+            <img onClick={() => clickCard(card.id)} className="cardImg" key={index} src={card.src} id={card.id} alt={card.alt} />
+          )
+        })}
+      </div>
     </div>
   )
 }
