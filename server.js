7cd85f0e82e4cbbc6fb9d56bd68853b8f6a518e5
Miriam Kisos <87091443+MKisos30@users.noreply.github.com>
2022-10-16 14:36:17 +0300

    server updadte

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const app = express();
+const path = require('path')
 
 app.use(express.static('frontend/build'))
+
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, './frontend/build/index.html'), function(err) {
+      if (err) {
+        res.status(500).send(err)
+      }
+    })
+  })
 
 const PORT = 3500;
 app.listen(PORT, () => {
